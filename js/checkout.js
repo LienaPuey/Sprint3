f73273636c13c8067b0fb2113f826c448678b16d
LienaPuey <lienapuey@gmail.com>
2022-11-30 17:34:06 +0100

    checkout

--- js/checkout.js
+++ js/checkout.js
@@ -1,23 +1,51 @@
 
-// Exercise 6
+// Exercise 7
+var inputs = document.getElementsByClassName("form-control");
+var element;
+for (element in inputs){
+	element.classList.add('required');
+}
 function validate() {
 	var error = 0;
 	// Get the input fields
 	var fName = document.getElementById("fName");
+	var fLastN = document.getElementById("fLastN");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
-	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	if(fName.value == "" || fName.value.length < 3 || /^\s+$/.test(fName)){
+		fName.classList.add("is-invalid");
 		error++;
 	}
 
-	if(fEmail.value == ""){
+	if(fEmail.value == "" || fEmail.value.length < 3 || !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(fEmail))){
+		fEmail.classList.add("is-invalid");
 		error++;
 	}
+	
+	if(fLastN.value == "" || fLastN.value.length < 3 || /^\s+$/.test(fLastN)){
+		fLastN.classList.add("is-invalid");
+		error++;
+	}
+	
+	if(fAddress.value == "" || fAddress.value.length < 3){
+		fAddress.classList.add("is-invalid");
+		error++;
+	}
+	
+	if(fPassword.value == "" || fPassword.value.length < 3 || !fPassword.value.match(/^(?=.*[0-9])(?=.*[a-zA-Z])[a-zA-Z0-9]{3,8}$/)){
+		fPassword.classList.add("is-invalid");
+		error++;
+	}
+	
+	if(fPhone.value == "" || fPhone.value.length != 9 || isNaN(fPhone.value)){
+		fPhone.classList.add("is-invalid");
+		error++;
+	}
+		
 	 
 	if(error>0){
 		alert("Error");
